7469a385492bea2856e081cb2f30e8d12ba85339
Justin Hong <justin0404@berkeley.edu>
2022-01-21 08:05:59 -0800

    started About

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -2,6 +2,18 @@ import './about.css'
 
 export default function about(){
     return(
-        <div></div>
+        <div class = "about container">
+            <div class = "col-sm-5 ">
+                <div class = "aboutTitle">
+                    <span> Who we are</span>
+                </div>
+                <div class = "aboutDescription">
+                    <span>Founded in 2022, 4D Capital is an organization based in 
+                    UC Berkeley that focuses on cryptocurrency. Our expereinced members 
+                    focus on creating invaluable experiences for both our clients and
+                     our members etc etc etc </span>
+                </div>
+            </div>
+        </div>
     )
 }
