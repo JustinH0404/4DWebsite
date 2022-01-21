f783923caf31cb781732ff4dbe8fb8ef502862ac
tommyychen <thomasychen@berkeley.edu>
2022-01-21 11:51:16 -0800

    Nothing much

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -3,7 +3,8 @@ import './about.css'
 export default function about(){
     return(
         <div class = "about container">
-            <div class = "col-sm-5 ">
+            <div class="row">
+            <div class = "col-md-5 ">
                 <div class = "aboutTitle">
                     <span> Who we are</span>
                 </div>
@@ -13,6 +14,27 @@ export default function about(){
                     focus on creating invaluable experiences for both our clients and
                      our members etc etc etc </span>
                 </div>
+            </div>
+            <div class= "col-md-7 ml-5">
+                <div class="row mt-5"> 
+                    <div class="col-sm-3 mb-5">
+                        <h1 class="stats1"> </h1>
+                        <h5 class="statdesccription">Dollars Raised </h5>
+                    </div>
+                    <div class="col-sm-3 mb-5">
+                        <h1 class="stats2"> </h1>
+                        <h5 class="statdesccription"> Members </h5>
+                    </div> 
+                    <div class="col-sm-3 mb-5">
+                        <h1 class="stats3"> </h1>
+                        <h5 class="statdesccription"> Projects</h5>
+                    </div> 
+                    <div class="col-sm-3 mb-5">
+                        <h1 class="stats4">1</h1>
+                        <h5 class="statdesccription"> Community </h5>
+                    </div>                   
+                </div>
+            </div>
             </div>
         </div>
     )
